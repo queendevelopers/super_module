52cbb9509c3fec04eb1d2e009560fe3681ee6e61
Dev <dev@Devs-MacBook-Pro.local>
2022-02-02 14:19:02 +0545

    added shared files

---
+++ lib/src/features/test/test_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'test_event.dart';
+part 'test_state.dart';
+
+class TestBloc extends Bloc<TestEvent, TestState> {
+  TestBloc() : super(TestInitial()) {
+    on<TestEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
