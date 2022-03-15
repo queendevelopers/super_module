90337139e812bc5023ff3f0510edc3cb116df951
deepak <deepak@goswivt.com.np>
2022-03-15 14:40:36 +0545

    country picker widget added

---
+++ lib/src/core/widget/app_country_picker.dart
@@ -0,0 +1,40 @@
+import 'package:carrier_info/carrier_info.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+
+import 'package:flutter/material.dart';
+
+class AppCountryPicker extends StatefulWidget {
+  final ValueChanged<CountryCode?>? onCountryPicked;
+
+  AppCountryPicker({required Key key, this.onCountryPicked}) : super(key: key);
+
+  @override
+  _AppCountryPickerState createState() => _AppCountryPickerState();
+}
+
+class _AppCountryPickerState extends State<AppCountryPicker> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: CarrierInfo.isoCountryCode,
+      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+        if (snapshot.hasData) {
+          return CountryCodePicker(
+            onChanged: widget.onCountryPicked,
+            initialSelection: snapshot.data,
+            showFlagDialog: false,
+            onInit: widget.onCountryPicked,
+          );
+        } else {
+          return CountryCodePicker(
+            onChanged: widget.onCountryPicked,
+            initialSelection:
+                WidgetsBinding.instance?.window.locale.countryCode,
+            showFlagDialog: false,
+            onInit: widget.onCountryPicked,
+          );
+        }
+      },
+    );
+  }
+}
