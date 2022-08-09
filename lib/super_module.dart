7fcf2a7a241a3bc6071f32970b37b82a1014d9e0
santoshacharya2468 <santoshacharya2468@gmail.com>
2022-08-09 12:36:34 +0545

    missing-export file added

--- lib/super_module.dart
+++ lib/super_module.dart
@@ -43,6 +43,8 @@ export 'package:super_module/src/features/cart/presentation/bloc/cart_bloc.dart'
 export 'package:super_module/src/features/chat/domain/controller/chat_controller.dart';
 export 'package:super_module/src/features/chat/domain/controller/chat_controller.dart';
 export 'package:super_module/src/features/chat/presentation/bloc/chat_bloc.dart';
+export 'package:super_module/src/features/firebase/domain/controller/firebase_controller.dart';
+export 'package:super_module/src/features/firebase/presentation/bloc/firebase_bloc.dart';
 export 'package:super_module/src/features/firebase/presentation/bloc/firebase_bloc.dart';
 export 'package:super_module/src/features/firebase/presentation/widget/update_alert_dialog.dart';
 export 'package:super_module/src/features/user/data/model/address_model.dart';
